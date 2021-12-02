08001f2f915ef0ea6b73dbb7f21c3bcbbe2e9eff
polarbear23 <xkyd96@gmail.com>
2021-12-02 17:21:57 +0000

    component created

---
+++ src/StoreItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StoreItem = () => {
+    return (
+        <li>
+            <div class="store--item-icon">
+                <img src="/assets/icons/001-beetroot.svg" alt="beetroot" />
+            </div>
+            <button>Add to cart</button>
+        </li>
+    )
+}
+
+export default StoreItem
