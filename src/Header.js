3e99f89cd93c05bc35ce54cba09ae98a14f37b2b
DWAUUD BELKO <belkodawuud@gmail.com>
2021-12-03 14:00:22 +0100

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Header = () => {
+  const [StoreItems, setStoreItems] = useState(initialStoreItems)
   return (
     <header id="store">
       <h1>Greengrocers</h1>
