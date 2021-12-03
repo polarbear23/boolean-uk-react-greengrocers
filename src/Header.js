a3c124b6acb671824997524da685672c4ba0405a
DWAUUD BELKO <belkodawuud@gmail.com>
2021-12-03 18:52:50 +0100

    new changes

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import StoreItem from './StoreItem'
+import { useState } from 'react'
 const Header = props => {
-  const [StoreItems, setStoreItems] = useState(initialStoreItems)
+  const [storeItems, setStoreItems] = useState(props.store)
   return (
     <header id="store">
       <h1>Greengrocers</h1>
-      <ul class="item-list store--item-list"></ul>
+      <ul class="item-list store--item-list">
+        {storeItems.map(item => {
+          return <StoreItem item={item} />
+        })}
+      </ul>
     </header>
   )
 }
 
 export default Header
+//import the data from db.js
+//use the data to declare a state veraible
+//map over the data and create a store item for every data object.
+// place the map inside the ul.
