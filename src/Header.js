cf561458397c2cdb778c62b5d903caf3e97b383d
DWAUUD BELKO <belkodawuud@gmail.com>
2021-12-03 14:09:50 +0100

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import StoreItem from './StoreItem'
-const Header = (props) => {
-const [StoreItems, setStoreItems] = useState(initialStoreItems)  return (
+const Header = props => {
+  const [StoreItems, setStoreItems] = useState(initialStoreItems)
+  return (
     <header id="store">
       <h1>Greengrocers</h1>
       <ul class="item-list store--item-list"></ul>
